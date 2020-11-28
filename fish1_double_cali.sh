f8b32d145f52f94a78fe4c913f28bcc00bf58e57
GitZzw <120137016@qq.com>
2020-11-28 10:16:09 +0800

    remove txts

--- fish1_double_cali.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-source /home/hjk/Desktop/zzw/catkin_new/devel/setup.sh
-roscore & sleep 2	
-### zzw_nodes_run
-cd /home/hjk/Desktop/zzw/catkin_new/src/camera_new/scripts
-python /home/hjk/Desktop/zzw/catkin_new/src/camera_new/scripts/t265_demo.py & sleep 2
-
-roslaunch camera_new camera_detect.launch 
-exit 0
--- fish2_once_cali.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-source /home/hjk/Desktop/zzw/cat/devel/setup.sh
-roslaunch realsense2_camera rs_t265.launch & sleep 3	
-### zzw_nodes_run
-roslaunch cat camera_detect.launch 
-exit 0
